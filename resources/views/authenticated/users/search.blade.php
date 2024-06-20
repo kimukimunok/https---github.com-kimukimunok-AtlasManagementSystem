a938169d2ae245f04f3fa8be22bd61b764dac09d
kimuranaoki <kimukimu.nok@gmail.com>
2024-06-20 23:59:03 +0900

    プロフィール詳細画面まで

--- resources/views/authenticated/users/search.blade.php
+++ resources/views/authenticated/users/search.blade.php
@@ -7,7 +7,7 @@
         @foreach($users as $user)
         <div class="border one_person">
             <div>
-                <span>ID : </span><span>{{ $user->id }}</span>
+                <span>ID : </span><span><b>{{ $user->id }}</b></span>
             </div>
             <div><span>名前 : </span>
                 <a href="{{ route('user.profile', ['id' => $user->id]) }}">
@@ -17,30 +17,30 @@
             </div>
             <div>
                 <span>カナ : </span>
-                <span>({{ $user->over_name_kana }}</span>
-                <span>{{ $user->under_name_kana }})</span>
+                <span><b>({{ $user->over_name_kana }} </b></span>
+                <span><b>{{ $user->under_name_kana }}) </b></span>
             </div>
             <div>
                 @if($user->sex == 1)
-                <span>性別 : </span><span>男</span>
+                <span>性別 : </span><span><b>男</b></span>
                 @elseif($user->sex == 2)
-                <span>性別 : </span><span>女</span>
+                <span>性別 : </span><span><b>女</b></span>
                 @else
-                <span>性別 : </span><span>その他</span>
+                <span>性別 : </span><span><b>その他</b></span>
                 @endif
             </div>
             <div>
-                <span>生年月日 : </span><span>{{ $user->birth_day }}</span>
+                <span>生年月日 : </span><span><b>{{ $user->birth_day }}</b></span>
             </div>
             <div>
                 @if($user->role == 1)
-                <span>権限 : </span><span>教師(国語)</span>
+                <span>権限 : </span><span><b>教師(国語)</b></span>
                 @elseif($user->role == 2)
-                <span>権限 : </span><span>教師(数学)</span>
+                <span>権限 : </span><span><b>教師(数学)</b></span>
                 @elseif($user->role == 3)
-                <span>権限 : </span><span>講師(英語)</span>
+                <span>権限 : </span><span><b>講師(英語)</b></span>
                 @else
-                <span>権限 : </span><span>生徒</span>
+                <span>権限 : </span><span><b>生徒</b></span>
                 @endif
             </div>
             <div>
@@ -58,36 +58,43 @@
     </div>
     <!-- ここユーザー検索 -->
     <div class="search_area w-25 border">
-        <div class="">
-            <div>
+        <div class="search_container">
+            <div class="search_top">
                 <p>検索</p>
                 <input type="text" class="free_word" name="keyword" placeholder="キーワードを検索" form="userSearchRequest">
             </div>
-            <div>
-                <lavel>カテゴリ</lavel>
+            <div class="search_category">
+                <label>カテゴリ</label>
                 <select form="userSearchRequest" name="category">
                     <option value="name">名前</option>
                     <option value="id">社員ID</option>
                 </select>
             </div>
-            <div>
+            <div class="search_category">
                 <label>並び替え</label>
                 <select name="updown" form="userSearchRequest">
                     <option value="ASC">昇順</option>
                     <option value="DESC">降順</option>
                 </select>
             </div>
-            <div class="">
-                <!-- 右側サイドバーの検索条件側 -->
-                <p class="m-0 search_conditions"><span>検索条件の追加</span></p>
+            <!-- 右側サイドバーの検索条件側 -->
+            <div class="sidebar_search">
+                <p class="m-0 search_conditions accordion-push-js ">
+                    <span class="search_border accordion-push">検索条件の追加
+                    </span>
+                </p>
                 <div class="search_conditions_inner">
                     <div>
                         <label>性別</label>
-                        <span>男</span><input type="radio" name="sex" value="1" form="userSearchRequest">
-                        <span>女</span><input type="radio" name="sex" value="2" form="userSearchRequest">
-                        <span>その他</span><input type="radio" name="sex" value="3" form="userSearchRequest">
+                        <p class="search_sex">
+                            <span><b>男</b></span><input type="radio" name="sex" value="1" form="userSearchRequest">
+                            <span><b>女</b></span><input type="radio" name="sex" value="2" form="userSearchRequest">
+                            <span><b>その他</b></span><input type="radio" name="sex" value="3" form="userSearchRequest">
+                        </p>
                     </div>
-                    <div>
+
+
+                    <div class="search_role">
                         <label>権限</label>
                         <select name="role" form="userSearchRequest" class="engineer">
                             <option selected disabled>----</option>
@@ -97,30 +104,25 @@
                             <option value="4" class="">生徒</option>
                         </select>
                     </div>
+
                     <div class="selected_engineer">
                         <label>選択科目</label>
-                        <!-- 検索条件欄に選択科目の表示を追加 -->
-                        <!-- 取得方法はforeachを使うため一覧画面と同じような記述になる？ -->
-                        <!-- 選択科目全部を表示させて、 チェックボックスで選択できるようにする。-->
-                        @foreach($AllSubjects as $subject)
+                    </div>
+                    @foreach($AllSubjects as $subject)
+                    <div class="search_subject">
                         <label>{{ $subject->subject }}
                             <input type="checkbox" name="subject_id[]" value=" {{ $subject->id }}" form="userSearchRequest">
-                            <!-- データを送るにはname属性に注意が必要。適切に設定しないと連想配列としてデータを送れない。 -->
-                            <!-- 今回のミス、name属性を暮らすか何かと勘違いして、記述しないでいた。→間違い。
-                        name属性はフィールドの名前を指定するものとなるためこれが記入されていないとそもそもデータを受け渡しできない。
-                    なので、ここでは選択科目識別のIDのsubject_idがフィールド名なので記述される。-->
-                            <!-- name属性の後につく[]とは？ これを見ろ→https://zoomyc.com/blog/archives/2656/
-                        複数選択が可能になるときはname属性のあとに[]をつける！！今回は選択科目は複数選択可能のため[]をつけなければならなかった。-->
-                            <!--  -->
-                        </label>
-                        @endforeach
                     </div>
+                    </label>
+                    @endforeach
                 </div>
             </div>
-            <div>
+
+
+            <div class="search_btn">
                 <input type="submit" name="search_btn" value="検索" form="userSearchRequest">
             </div>
-            <div>
+            <div class="search_reset">
                 <input type="reset" value="リセット" form="userSearchRequest">
             </div>
         </div>
